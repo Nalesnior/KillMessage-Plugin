792374156125e7971bb884d5e42d04128762fc39
Naleśnior <lisquxdd@gmail.com>
2025-10-23 21:05:55 +0200

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.API.Interfaces;
+using System.Collections.Generic;
+
+namespace KillInfoPlugin
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        // Format of kill message
+        //   {killCount}   - Count of kills
+        //   {victim}      - Nickname of the victim
+        //   {victimRole}  - Role of the victim
+        public string KillMessageFormat { get; set; } = "You have {killCount} kills. You killed {victim} ({victimRole}).";
+        public string SucideMessageFormat { get; set; } = "Congratulations! You killed yourself!";
+        public float MessageDuration { get; set; } = 5f;
+        public bool Debug { get; set; } = false;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Exiled.Events.EventArgs.Player;
+using System;
+
+namespace KillInfoPlugin
+{
+    public class KillInfoPlugin : Plugin<Config>
+    {
+        private readonly Dictionary<string, int> _killCounts = new Dictionary<string, int>();
+
+        public override string Name => "KillInfoPlugin";
+        public override string Author => "Naleśnior";
+        public override Version Version => new Version(3, 0, 0);
+        public override string Prefix => "killinfo";
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Dying += OnPlayerDying;
+            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Dying -= OnPlayerDying;
+            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+            base.OnDisabled();
+        }
+
+        private void OnRoundStarted()
+        {
+            _killCounts.Clear();
+        }
+
+        private void OnPlayerDying(DyingEventArgs ev)
+        {
+            Player victim = ev.Player;
+            Player killer = ev.Attacker;
+
+            if (killer == null || killer == victim)
+            {
+                victim.ShowHint(Config.SucideMessageFormat, this.Config.MessageDuration);
+                return;
+            }
+
+            if (!_killCounts.ContainsKey(killer.UserId))
+                _killCounts[killer.UserId] = 0;
+
+            _killCounts[killer.UserId]++;
+            int totalKills = _killCounts[killer.UserId];
+
+            string victimName = victim.Nickname;
+            string victimRole = victim.Role.ToString();
+
+            string message = Config.KillMessageFormat
+                .Replace("{killCount}", totalKills.ToString())
+                .Replace("{victim}", victimName)
+                .Replace("{victimRole}", victimRole);
+
+            killer.ShowHint(message, this.Config.MessageDuration);
+        }
+    }
+}
+
